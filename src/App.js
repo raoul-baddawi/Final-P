481d868bc4054b7428cecc650e698b2c9df338d7
raoul-baddawi <raoulbaddawi1@gmail.com>
2023-04-29 09:48:18 +0300

    basic structuring finished

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
 import './App.css';
+import { Route, Routes, BrowserRouter } from "react-router-dom";
+import Layout from './Maintainer/Layout';
+import Home from './Pages/Home/Home';
 
 function App() {
   return (
     <>
+      <BrowserRouter>
+        <Routes>
+          <Route element={<Layout />}>
+            <Route path="/" element={<Home />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
